1d6fea68e61b9f3023f8f501708dfa5d6d6a175f
AlbeCamp21 <luis.alanya.c@uni.pe>
2024-11-17 20:17:10 -0500

    pantalla de transcripción agregada

---
+++ app/src/main/java/com/grupo2/audiohelp/TranscriptionActivity_2.java
@@ -0,0 +1,12 @@
+package com.grupo2.audiohelp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TranscriptionActivity_2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.transcription_main_2); // Enlaza con transcription_main_2.xml
+    }
+}
