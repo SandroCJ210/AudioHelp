b6506b8f557dd5c88478e8c220c4c3e10f8aa8be
AlemEsv <cinver.espinoza.v@uni.pe>
2024-12-07 21:38:15 -0500

    creación de login y registro de cuentas usando firebase

--- app/src/main/java/com/grupo2/audiohelp/LoginActivity.java
+++ app/src/main/java/com/grupo2/audiohelp/LoginActivity.java
@@ -2,27 +2,88 @@ package com.grupo2.audiohelp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth autorizador;
+    private EditText loginEmail, loginPassword;
+    private TextView registroRedirect;
+    private Button loginBoton;
+    private ImageButton btnRetroceder;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inicio_sesion);
 
-        Button BtonLocucion = findViewById(R.id.Registrarse);
-        BtonLocucion.setOnClickListener(w -> {
-            Intent intent = new Intent(LoginActivity.this, RegistroActivity.class);
-            startActivity(intent);
+        btnRetroceder = findViewById(R.id.regresarOpciones);
+        autorizador = FirebaseAuth.getInstance();
+        loginEmail = findViewById(R.id.login_email);
+        loginPassword = findViewById(R.id.login_password);
+        loginBoton = findViewById(R.id.login_btn);
+        registroRedirect = findViewById(R.id.registroRedirect);
+
+        loginBoton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String email = loginEmail.getText().toString();
+                String password = loginPassword.getText().toString();
+
+                if(!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    if(!password.isEmpty()){
+                        autorizador.signInWithEmailAndPassword(email,password)
+                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        Toast.makeText(LoginActivity.this, "Ingreso correcto.", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(LoginActivity.this, Menu_Opciones_activity.class));
+                                        finish();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(LoginActivity.this, "Inicio de sesión fallido.", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    }else{
+                        loginPassword.setError("La contraseña no puede estar vacía.");
+                    }
+                }else if(email.isEmpty()){
+                    loginEmail.setError("El Email no puede estar vacío.");
+                } else{
+                    loginEmail.setError("Ingrese un Email válido.");
+                }
+            }
         });
 
-        ImageButton btnRetroceder = findViewById(R.id.imageButton);
-        btnRetroceder.setOnClickListener(v -> {
-            Intent intent = new Intent(LoginActivity.this, Menu_Opciones_activity.class);
-            startActivity(intent);
+        registroRedirect.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(LoginActivity.this,RegistroActivity.class));
+            }
+        });
+
+        btnRetroceder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(LoginActivity.this, Menu_Opciones_activity.class));
+            }
         });
 
     }
--- app/src/main/java/com/grupo2/audiohelp/RegistroActivity.java
+++ app/src/main/java/com/grupo2/audiohelp/RegistroActivity.java
@@ -2,17 +2,74 @@ package com.grupo2.audiohelp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegistroActivity extends AppCompatActivity {
+
+    private FirebaseAuth autorizador;
+    private EditText registroEmail,registroPassword;
+    private Button registroBoton;
+    private TextView loginRedirect;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.crear_cuenta);
 
+        autorizador = FirebaseAuth.getInstance();
+        registroEmail = findViewById(R.id.registro_email);
+        registroPassword = findViewById(R.id.registro_password);
+        registroBoton = findViewById(R.id.registro_btn);
+        loginRedirect = findViewById(R.id.loginRedirect);
 
+        registroBoton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String user = registroEmail.getText().toString().trim();
+                String password = registroPassword.getText().toString().trim();
+
+                if(user.isEmpty()){
+                    registroEmail.setError("Ingresa un Email valido.");
+                }
+
+                if(password.isEmpty()){
+                    registroEmail.setError("Ingrese una contraseña valida.");
+                } else{
+                    autorizador.createUserWithEmailAndPassword(user, password).addOnCompleteListener(new OnCompleteListener<AuthResult>(){
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task){
+                            if(task.isSuccessful()){
+                                Toast.makeText(RegistroActivity.this, "Registro completado.", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(RegistroActivity.this, LoginActivity.class));
+                            } else{
+                                Toast.makeText(RegistroActivity.this, "Registro fallido.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+
+        });
+
+        loginRedirect.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(RegistroActivity.this, LoginActivity.class));
+            }
+        });
     }
-
 }
