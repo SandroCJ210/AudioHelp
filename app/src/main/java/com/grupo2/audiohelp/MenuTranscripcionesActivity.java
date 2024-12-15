377a5d092d6ee60301a9208932327bcc6e214ad0
SandroCJ210 <sandro.carrillo.j@uni.pe>
2024-12-15 14:26:37 -0500

    fix: fix mic error when using transcription button

--- app/src/main/java/com/grupo2/audiohelp/MenuTranscripcionesActivity.java
+++ app/src/main/java/com/grupo2/audiohelp/MenuTranscripcionesActivity.java
@@ -2,19 +2,19 @@ package com.grupo2.audiohelp;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
-
+import android.Manifest;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import java.io.IOException;
 
 public class MenuTranscripcionesActivity extends AppCompatActivity {
-    private boolean isRecording = false;
-    private MediaRecorder recorder;
-    private String audioFilePath;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // Cargar tema antes de llamar a super.onCreate()
@@ -31,13 +31,10 @@ public class MenuTranscripcionesActivity extends AppCompatActivity {
 
         ImageButton btnTrans = findViewById(R.id.locution_button);
         btnTrans.setOnClickListener(v -> {
-            if (isRecording) {
-                stopRecording();
-                goToTranscriptionActivity();
-            } else {
-                startRecording();
-            }
+            Intent intent = new Intent(MenuTranscripcionesActivity.this, TranscripcionActivity.class);
+            startActivity(intent);
         });
+
         ImageButton btnHistorial = findViewById(R.id.saved_transcriptions_icon);
         btnHistorial.setOnClickListener(w -> {
             Intent intent = new Intent(MenuTranscripcionesActivity.this, GestorTranscripcionesActivity.class);
@@ -55,36 +52,6 @@ public class MenuTranscripcionesActivity extends AppCompatActivity {
             startActivity(intent);
             overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
         });
-    }
-
-    private void startRecording() {
-        audioFilePath = getExternalFilesDir(null) + "/audio_AudioHelp.wav";
-        recorder = new MediaRecorder();
-        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-        recorder.setOutputFile(audioFilePath);
-
-        try {
-            recorder.prepare();
-            recorder.start();
-            isRecording = true;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void stopRecording() {
-        if (isRecording) {
-            recorder.stop();
-            recorder.release();
-            isRecording = false;
-        }
-    }
-    private void goToTranscriptionActivity(){
-        Intent intent = new Intent(this, TranscripcionActivity.class);
-        intent.putExtra("audioFilePath", audioFilePath);
-        startActivity(intent);
     }
 
 }
